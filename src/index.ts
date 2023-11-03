4288a8bad3a3532e1bdfd4d2fa3b1c90afa3b0de
aziz-codes <azizisoffline@gmail.com>
2023-11-03 11:12:13 -0700

    using objects and optional keys

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+const obj = {
+  name: "aziz",
+  age: 25,
+  isAdmin: false,
+};
+obj.name = "Noor";
+
+let obj2: {
+  name: string;
+  age: number;
+  phoneNumber?: string | number;
+};
+
+obj2 = {
+  name: "aziz",
+  age: 32,
+};
