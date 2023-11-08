523adb95c275d453eb5d96b6bd2ef477789887c5
aziz-codes <azizisoffline@gmail.com>
2023-11-08 23:50:04 +0500

    grid system design

---
+++ src/components/Grid.tsx
@@ -0,0 +1,11 @@
+const Grid = () => {
+  return (
+    <div className="grid grid-cols-3">
+      <div className="bg-red-500">section 1</div>
+      <div className="bg-green-500">section 2</div>
+      <div className="bg-yellow-500">section 3</div>
+    </div>
+  );
+};
+
+export default Grid;
