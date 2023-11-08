0057b8de252c84ad24ba19d6ced3dba06a0009e3
aziz-codes <azizisoffline@gmail.com>
2023-11-08 23:49:54 +0500

    usign tailwind grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { useEffect, useState } from "react";
-import Posts from "./components/Todos";
+// import { useEffect, useState } from "react";
+// import Posts from "./components/Todos";
+
+import Grid from "./components/Grid";
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const getData = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  // const [data, setData] = useState([]);
+  // const getData = async () => {
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
 
-    const result = await res.json();
-    setData(result);
-  };
-  console.log(data);
-  useEffect(() => {
-    getData();
-  }, []);
-  return <Posts todos={data} />;
+  //   const result = await res.json();
+  //   setData(result);
+  // };
+  // console.log(data);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+  return <Grid />;
 };
 
 export default App;
